7588c9974f3a41ecbca598c99f7b635a20d354b3
C. Alan Zoppa <czoppa@enova.com>
2016-01-08 15:51:09 -0600

    unique constraint, basically

--- isalllike/tweets/models.py
+++ isalllike/tweets/models.py
@@ -13,7 +13,7 @@ api = TwitterAPI(**settings.TWITTER_AUTHENTICATION)
 class Tweet(models.Model):
     message = models.TextField(blank=True, null=True)
     user = models.CharField(max_length=255,)
-    twitter_id = models.CharField(max_length=255,)
+    twitter_id = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.user + ': ' + self.message
--- isalllike/tweets/tests.py
+++ isalllike/tweets/tests.py
@@ -2,14 +2,24 @@ from django.test import TestCase, TransactionTestCase
 import vcr
 from .models import Tweet
 
-# Create your tests here.
 
-class TwitterApiTest(TestCase):
-    #def test_gather_for_user(self):
-        #with vcr.use_cassette('tweets/vcr_cassettes/c_alan_zoppa_first_100.yml'):
-            #Tweet.gather_for_user('c_alan_zoppa')
-
+class TwitterApiTest(TransactionTestCase):
     def test_oldest_for_user(self):
+        assert Tweet.oldest_for_user('c_alan_zoppa') == None
+        Tweet.objects.create(
+                    message="arbitrary",
+                    user='c_alan_zoppa',
+                    twitter_id='2'
+        )
+        Tweet.objects.create(
+                    message="arbitrary",
+                    user='c_alan_zoppa',
+                    twitter_id='1'
+        )
+        assert Tweet.oldest_for_user('c_alan_zoppa') == '1'
+
+
+    def test_gather_for_user(self):
         with vcr.use_cassette('tweets/vcr_cassettes/page_in_history.yml', record_mode='new_episodes'):
             Tweet.gather_for_user('c_alan_zoppa')
-            Tweet.oldest_for_user('c_alan_zoppa')
+        assert Tweet.objects.count() == 315
