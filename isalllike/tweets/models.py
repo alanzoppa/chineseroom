13f56ee2af4956dbcc64276a2917d9dff9ee194d
C. Alan Zoppa <czoppa@enova.com>
2016-01-08 15:10:56 -0600

    basic vcr working

--- isalllike/tweets/models.py
+++ isalllike/tweets/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from django.db import transaction
+
+import ipdb
 
 # Create your models here.
 
@@ -10,7 +13,47 @@ api = TwitterAPI(**settings.TWITTER_AUTHENTICATION)
 class Tweet(models.Model):
     message = models.TextField(blank=True, null=True)
     user = models.CharField(max_length=255,)
+    twitter_id = models.CharField(max_length=255,)
+
+    def __str__(self):
+        return self.user + ': ' + self.message
+
+    class Meta:
+        ordering = ['-twitter_id']
+
+
+    @classmethod
+    def oldest_for_user(self, username):
+        oldest_tweet = Tweet.objects.order_by('-twitter_id').last()
+        if oldest_tweet:
+            return oldest_tweet.twitter_id
+        else:
+            return None
 
     @classmethod
     def gather_for_user(self, username):
-        return api.request('statuses/user_timeline', {'count': 100})
+        while Tweet.gather_older_for_user(username, Tweet.oldest_for_user(username)):
+            pass
+
+    @classmethod
+    def gather_older_for_user(self, username, before_this_id=None):
+        params = {'count': 100, 'include_rts': False,}
+        if before_this_id:
+            params['max_id'] = str(before_this_id)
+        request = api.request('statuses/user_timeline', params)
+        tweets = [t for t in request]
+
+        try:
+            Tweet.objects.get(twitter_id=tweets[-1]['id_str'])
+            return None
+        except Tweet.DoesNotExist:
+            pass
+
+        with transaction.atomic():
+            for t in tweets:
+                Tweet.objects.get_or_create(
+                    message=t['text'],
+                    user=username,
+                    twitter_id=t['id']
+                )
+        return True
--- isalllike/tweets/tests.py
+++ isalllike/tweets/tests.py
@@ -4,7 +4,12 @@ from .models import Tweet
 
 # Create your tests here.
 
-class TwitterApiTest(TransactionTestCase):
-    def test_gather_for_user(self):
-        with vcr.use_cassette('tweets/vcr_cassettes/c_alan_zoppa_first_100.yml'):
+class TwitterApiTest(TestCase):
+    #def test_gather_for_user(self):
+        #with vcr.use_cassette('tweets/vcr_cassettes/c_alan_zoppa_first_100.yml'):
+            #Tweet.gather_for_user('c_alan_zoppa')
+
+    def test_oldest_for_user(self):
+        with vcr.use_cassette('tweets/vcr_cassettes/page_in_history.yml', record_mode='new_episodes'):
             Tweet.gather_for_user('c_alan_zoppa')
+            Tweet.oldest_for_user('c_alan_zoppa')
