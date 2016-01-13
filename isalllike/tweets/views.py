51c073a50bfbb6614ff428694ad04cd39e4c916b
C. Alan Zoppa <czoppa@enova.com>
2016-01-13 13:48:46 -0600

    end-to-end works

--- isalllike/tweets/views.py
+++ isalllike/tweets/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
-from isalllike.tweets.models import NGram
+from isalllike.tweets.models import NGram, NovelParagraph
 import ipdb
 
 # Create your views here.
 
 def index(request):
-    if request.method == 'POST':
-        #post_dictionary = copy(request.POST)
-        sources = filter(lambda x: x.startswith('source-'), request.POST)
-        sources = [(s[7:], int(request.POST[s])) for s in sources]
-        assert False
-
     context = {
         'sources': [n.source for n in NGram.objects.all().distinct('source')],
     }
+
+
+    if request.method == 'POST':
+        #post_dictionary = copy(request.POST)
+        sources = filter(lambda x: x.startswith('source-'), request.POST)
+        sources = [(s[7:], int(request.POST[s])/100) for s in sources if request.POST[s] != '0']
+        paragraph = NovelParagraph(*sources)
+        paragraph.append_sentence()
+        paragraph.append_sentence()
+        paragraph.append_sentence()
+        context['sentences'] = paragraph.human_readable_sentences()
 
     return render(
         request=request,
