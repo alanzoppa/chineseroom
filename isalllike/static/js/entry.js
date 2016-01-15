8c002061a3413c2d8133008c4d58064120071c1e
C. Alan Zoppa <czoppa@enova.com>
2016-01-15 10:48:50 -0600

    persistence and other UI tweaks

--- isalllike/static/js/entry.js
+++ isalllike/static/js/entry.js
@@ -1,29 +1,42 @@
-$(document).ready(function() {
-
-$('.source-slider').each( function(index) {
-        $(this).prev('span').html(this.value);
-    })
-
-});
-
 $(document).on('change, mousemove', '.source-slider', function(e){
     $(e.currentTarget).prev('span').html(e.currentTarget.value);
 });
 
-$(document).on('change', '.source-slider', function(e){
+
+var foo = function(e){
     var sliders = $('.source-slider');
     var values = sliders.map( function(i) { return parseInt(this.value) });
     var total = Array.prototype.reduce.call(
             values, function(a,b){return a+b}, 0
             );
     sliders.each(
-            function(){ $(this).attr('max', 100-total+parseInt(this.value)); }
+            function(){
+                console.log('foo');
+                console.log(100-total+parseInt(this.value));
+                $(this).attr('max', 100-total+parseInt(this.value));
+            }
             )
-    $("#totals").html(total);
     if (total == 100) { 
         $('#submit-run').removeAttr('disabled');
     }
     else {
         $('#submit-run').attr('disabled', 'disabled');
     }
+    $('#submit-run').val(
+            '('+ total + ' / 100) Submit'
+            )
+}
+
+
+
+$(document).on('change', '.source-slider',  foo);
+
+$(document).ready(function() {
+
+$('.source-slider').each( function(index) {
+        $(this).prev('span').html(this.value);
+    })
+
+foo();
+
 });
--- isalllike/tweets/views.py
+++ isalllike/tweets/views.py
@@ -5,13 +5,13 @@ import ipdb
 # Create your views here.
 
 def index(request):
-    context = {
-        'documents': Document.objects.all(),
-        'twitter_users': TwitterUser.objects.all()
-    }
+    context = {}
     if request.method == 'POST':
+        context.update(_get_sources(request.POST))
         paragraph = NovelParagraph( *_extract_probabilities(request.POST) )
         context['sentences'] = _generate_markov_string(paragraph)
+    else:
+        context.update(_get_sources())
 
     return render(
         request=request,
@@ -26,6 +26,27 @@ def _extract_probabilities(data):
         (s[len(prefix):], int(data[s])/100)
         for s in sources if data[s] != '0'
     ]
+
+def _get_sources(data={}):
+    documents = Document.objects.all()
+    twitter_users = TwitterUser.objects.all()
+    for document in documents:
+        key = 'source-document:'+document.name
+        if key in data:
+            document.ratio = data[key]
+        else:
+            document.ratio = 0
+    for twitter_user in twitter_users:
+        key = 'source-'+twitter_user.twitter_id+'@twitter'
+        if key in data:
+            twitter_user.ratio = data[key]
+        else:
+            twitter_user.ratio = 0
+    return {
+        'documents': documents,
+        'twitter_users': twitter_users
+    }
+
 
 def _generate_markov_string(novel_paragraph):
     novel_paragraph.append_sentence()
